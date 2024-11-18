ac07db228736459da85a86dfa271c4d9231dd960
Even <even.berhane2022@gmail.com>
2024-11-18 15:37:33 +0300

    reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,23 @@
-import { ThemeProvider } from "../context/ThemeContext";
-import ThemeToggle from "../components/ThemeToggle";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Russom",
   description: "Portfolio",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>
-        <ThemeProvider>
-          {/* Dynamic Background */}
-          <div className="relative h-full w-full">
-            <div className={`absolute inset-0 -z-10 ${theme === "dark" ? "bg-black" : "bg-white"}`}>
-              {theme === "dark" ? (
-                <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-                  <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
-                </div>
-              ) : (
-                <div className="absolute top-0 -z-10 h-full w-full bg-white">
-                  <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
-                </div>
-              )}
-            </div>
-            {/* Content */}
-            <ThemeToggle />
-            {children}
-          </div>
-        </ThemeProvider>
+      <body
+        className="relative"
+      >
+        {children}
       </body>
     </html>
   );
