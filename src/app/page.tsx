563bc644497fd7c7afd75055f080bc3c5cc32c41
Even <even.berhane2022@gmail.com>
2024-11-19 18:40:00 +0300

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import BlurIn from "@/component/BlurIn";
+import NavBar from "@/component/NavBar";
 import { WordPullUp } from "@/component/WordPullUp";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative">
+    
+      <NavBar/>
       <section id="home" className="felx flex-row">
         <div className="flex flex-col gap-2 items-start text-center h-screen justify-center">
           <WordPullUp words="Hi, Im Russom" className="text-6xl  text-gray-50 font-publicSans"/>
@@ -15,7 +18,7 @@ export default function Home() {
         </div>
       </section>
       <section id="work" className="">
-          
+        
       </section>
       <section id="projects">
 
