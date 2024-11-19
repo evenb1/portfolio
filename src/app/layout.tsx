bf9792db880d801eef98627ae052e68453cefae9
Even <even.berhane2022@gmail.com>
2024-11-19 18:12:54 +0300

    globe json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="relative"
+        className=" h-max"
       >
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         {children}
+
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import BlurIn from "@/component/BlurIn";
+import { WordPullUp } from "@/component/WordPullUp";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-     <h1>Portfolio</h1>
-    </div>
+    <main className="relative">
+      <section id="home" className="felx flex-row">
+        <div className="flex flex-col gap-2 items-start text-center h-screen justify-center">
+          <WordPullUp words="Hi, Im Russom" className="text-6xl  text-gray-50 font-publicSans"/>
+          <BlurIn word="Web Developer" className="text-gray-100 font-publicSans text-6xl"/>
+        </div>
+        <div>
+          
+        </div>
+      </section>
+      <section id="work" className="">
+          
+      </section>
+      <section id="projects">
+
+      </section>
+      <section id="contact">
+
+      </section>
+    </main>
   );
 }
